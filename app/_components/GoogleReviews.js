9acfab7be414abadd4fbb16e4374ff2e75157b4e
Cédric Phung <cedric3011@outlook.fr>
2025-04-23 15:39:12 +0200

    add section title

--- app/_components/GoogleReviews.js
+++ app/_components/GoogleReviews.js
@@ -4,6 +4,9 @@ function GoogleReviews() {
   return (
     <>
       <div className="mt-5">
+        <h2 className="text-center text-[1.8em] text-stone-700 font-semibold mb-3">
+          Témoignages Google
+        </h2>
         <div
           className="elfsight-app-7da455a3-c7cb-4198-97b4-0adeb748709d"
           data-elfsight-app-lazy
